f777ea7e73bd2a0c83a13d0cc5586e687b4c76f7
leechiahwa <leechiahwa99@hotmail.com>
2024-11-05 23:36:01 +0800

    feat: add handleScroll utility function for smooth scrolling to sections

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const handleScroll = (sectionId: string) => {
+  const aboutSection = document.getElementById(sectionId)
+  aboutSection?.scrollIntoView({ behavior: 'smooth' })
+}
