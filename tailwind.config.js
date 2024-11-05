a8e59dadc539ea04817831ab4a2637d192865eca
leechiahwa <leechiahwa99@hotmail.com>
2024-11-05 23:26:13 +0800

    feat: add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -60,10 +60,40 @@ module.exports = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: 0 },
         },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeInFromRight: {
+          '0%': { opacity: '0', transform: 'translateX(30px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeInFromLeft: {
+          '0%': { opacity: '0', transform: 'translateX(-200px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        letterFade: {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        bounce: {
+          '0%, 100%': { transform: 'translateY(-5%)' },
+          '50%': { transform: 'translateY(0)' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'fade-in-right': 'fadeInFromRight 1s ease-in-out forwards',
+        'fade-in-left': 'fadeInFromLeft 1s ease-in-out forwards',
+        'fade-in': 'fadeIn 1s ease-in-out forwards',
+        'letter-fade': 'fadeIn 0.5s ease-in-out forwards',
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        bounce: 'bounce 1s ease-in-out infinite',
       },
     },
   },
