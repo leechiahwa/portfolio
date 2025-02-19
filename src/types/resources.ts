d39164524119b40c167948397f0028717a8fbd49
leechiahwa <leechiahwa99@hotmail.com>
2025-02-19 12:33:11 +0800

    chore: remove unused translations

--- src/types/resources.ts
+++ src/types/resources.ts
@@ -1,9 +1,7 @@
-import translation from '../i18n/locales/en/translation.json'
 import notfound from '../i18n/locales/en/notfound.json'
 
 const resources = {
   notfound,
-  translation,
 } as const
 
 export default resources
